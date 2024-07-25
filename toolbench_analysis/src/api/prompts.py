66fd8634c87c0f95034454fe161b52e6d7b3b592
woojeong-merlyn <woojeong.kim@merlyn.org>
2024-07-25 14:05:48 -0400

    add API_INTENT_PROMPT

--- toolbench_analysis/src/api/prompts.py
+++ toolbench_analysis/src/api/prompts.py
@@ -7,3 +7,15 @@ API_SUMMARY_PROMPT = """I will provide you information about an api, including t
 Please produce a natural language summary describing the API's capabilities. Your summary should be around 200 words. 
 If you're familiar with the underlying web-service (like an Amazon API or weather.com API), briefly describe it.
 Your description should enable someone who doesn't know about the API to understand when they should use the API."""
+
+# NOTE make it shorter or focus on the user intent rather than api description
+API_INTENT_PROMPT = """I will provide you information about an API, including the name, description, and input parameters. 
+There are two types of parameters: required and optional. Required parameters are necessary for the API to function, while optional parameters are not.
+You can figure out this information by looking at each parameter's type. I will provide this information in a json format. 
+There APIs can be used to solve queries given by users.
+
+Please produce a natural language summary describing the API's capabilities. Your summary should be around 200 words. 
+If you're familiar with the underlying web-service (like an Amazon API or weather.com API), briefly describe it.
+Your description should enable someone who doesn't know about the API to understand when they should use the API.
+Please provide a brief description of the API's intended use case.
+"""
